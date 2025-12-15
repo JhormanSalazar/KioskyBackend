d2ead7353f60a2b9caabd0b66fc373e95074d1f9
JhormanSalazar <salazarjhorman181@gmail.com>
2025-12-15 17:57:17 -0500

    first commit

---
+++ src/main/java/com/kiosky/kiosky/KioskyApplication.java
@@ -0,0 +1,13 @@
+package com.kiosky.kiosky;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KioskyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KioskyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kiosky/kiosky/KioskyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kiosky.kiosky;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KioskyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
