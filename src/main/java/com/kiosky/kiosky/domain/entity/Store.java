4dea1bb660526bc4604680a125a4de7f85f0cf9e
JhormanSalazar <salazarjhorman181@gmail.com>
2026-02-08 15:32:31 -0500

    feat: Remove unused import statement from Store entity

--- src/main/java/com/kiosky/kiosky/domain/entity/Store.java
+++ src/main/java/com/kiosky/kiosky/domain/entity/Store.java
@@ -9,8 +9,6 @@ import org.hibernate.annotations.JdbcTypeCode;
 import org.hibernate.type.SqlTypes;
 
 import java.util.List;
-import java.util.stream.Stream;
-
 @Getter
 @Setter
 @AllArgsConstructor
